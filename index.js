bc77602a566f6b4a1533761284a81894689f908f
pingingin <86203918+pingingin@users.noreply.github.com>
2022-07-14 07:56:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -224,7 +224,7 @@ wsServer.on("request", request => {
 })
 
 function S4() {
-    return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
+    return (((1+Math.random())*0x10000)|0).toString(4).substring(1); 
 }
  
 // then to call it, plus stitch in '4' in the third group
